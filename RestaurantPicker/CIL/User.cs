e0b67b715e882e8aea1763837bb19347d076fb26
iik0001 <iik0001@uah.edu>
2018-11-06 23:23:48 -0600

    Added DTO and DAL -- making sure connection is working

---
+++ RestaurantPicker/CIL/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class User
+    {
+        public Int16 UserID { get; set; }
+        public String Fname { get; set; }
+        public String Lname { get; set; }
+        public String Email { get; set; }
+        public String Zipcode { get; set; }
+    }
+}
